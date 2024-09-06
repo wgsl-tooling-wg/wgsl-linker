5b849759c00d09fb72aa0cd09b7d5d435b7c7106
lee mighdoll <lee@underneath.ca>
2024-09-05 23:43:28 -0700

    mv some import parsing tests over to cases

--- packages/linker/src/test/GleamImport.test.ts
+++ packages/linker/src/test/GleamImport.test.ts
@@ -1,69 +1,13 @@
 import { testParse, TestParseResult } from "mini-parse/test-util";
 import { expect, TaskContext, test } from "vitest";
 import { gleamImport } from "../GleamImport.js";
-import { dlog } from "berry-pretty";
-
-function expectParseFail(ctx: TaskContext): void {
-  const failPrefix = "bad: ";
-  const src = ctx.task.name.slice(failPrefix.length);
-  const result = testParse(gleamImport, src);
-  expect(result.parsed).is.null;
-}
 
 function expectParses(ctx: TaskContext): TestParseResult<void> {
   const result = testParse(gleamImport, ctx.task.name);
   expect(result.parsed).is.not.null;
   return result;
 }
-/* ------  failure cases  -------   */
-
-test("bad: import", expectParseFail);
-test("bad: import foo", expectParseFail);
-test("bad: import ./foo", expectParseFail);
-test("bad: import ../foo", expectParseFail);
-test("bad: import ./foo /bar", expectParseFail);
-test("bad: import .", expectParseFail);
-test("bad: import ./", expectParseFail);
-test("bad: import ../", expectParseFail);
-test("bad: import ../.", expectParseFail);
-test("bad: import ../..", expectParseFail);
-test("bad: import foo/{*}", expectParseFail);
-test("bad: import foo/*/b", expectParseFail);
-test("bad: import foo/{}", expectParseFail);
-test("bad: import foo/../bar/baz", expectParseFail);
-test("bad: import foo/bee as boo/bar", expectParseFail);
-
 /* ------  success cases  -------   */
-
-test("import ./foo/bar", expectParses);
-test("import ../../foo/bar", expectParses);
-test(`import ../b/c/d`, expectParses);
-test(`import a/b/c`, expectParses);
-test("import foo/bar", expectParses);
-test("import foo/{a,b}", expectParses);
-test("import foo/{a, b}", expectParses);
-test("import foo/bar/{a, b}", expectParses);
-test("import a/{b, c }", expectParses);
-test("import foo/* as b", expectParses);
-test("import foo/a as b", expectParses);
-test(`import a/b/{c as foo}`, expectParses);
-
-test(
-  `
-  import ./foo/bar
-  fn main() {}
-  `,
-  expectParses
-);
-
-test(
-  `import bevy_pbr/{
-  mesh_view_bindings,
-  utils/{PI, noise},
-  lighting/*
-}`,
-  expectParses
-);
 
 test("import ./foo/bar;", (ctx) => {
   const result = expectParses(ctx);
---
+++ packages/linker/src/test/ImportSyntaxCases.test.ts
@@ -0,0 +1,24 @@
+import { testParse, TestParseResult } from "mini-parse/test-util";
+import { expect, test } from "vitest";
+import { importSyntaxCases } from "../../../shared-tests/src/test-cases/ImportSyntaxCases.js";
+import { gleamImport } from "../GleamImport.js";
+
+function expectParseFail(src: string): void {
+  const result = testParse(gleamImport, src);
+  expect(result.parsed).is.null;
+}
+
+function expectParses(src: string): TestParseResult<void> {
+  const result = testParse(gleamImport, src);
+  console.log(src);
+  expect(result.parsed).is.not.null;
+  return result;
+}
+
+  importSyntaxCases.forEach((c) => {
+    if (c.fails) {
+      test(c.src, () => expectParseFail(c.src));
+    } else {
+      test(c.src, () => expectParses(c.src));
+    }
+  });
