51b42b4ee2a2f29cd1ed6e85b014e2dd07c77b4b
lee mighdoll <lee@underneath.ca>
2024-10-26 18:30:19 -0700

    include WgslBundle in package export

--- packages/linker/src/index.ts
+++ packages/linker/src/index.ts
@@ -3,4 +3,5 @@ export * from "./ModuleRegistry.js";
 export * from "./ParseWgslD.js";
 export * from "./PathUtil.js";
 export * from "./Util.js";
+export * from "./WgslBundle.js";
 export { preProcess } from "./ParseModule.js";
