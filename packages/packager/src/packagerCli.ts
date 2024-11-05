9a403b7705338d1e3c060cc44bbebe936ccced74
lee mighdoll <lee@underneath.ca>
2024-11-04 17:35:50 -0800

    rm NYI feature

--- packages/packager/src/packagerCli.ts
+++ packages/packager/src/packagerCli.ts
@@ -1,5 +1,4 @@
 import yargs from "yargs";
-import fs from "fs";
 import { packageWgsl } from "./packageWgsl.js";
 
 export type CliArgs = ReturnType<typeof parseArgs>;
@@ -11,28 +10,30 @@ export async function packagerCli(rawArgs: string[]): Promise<void> {
 }
 
 function parseArgs(args: string[]) {
-  return yargs(args)
-    .command("$0", "create an npm package from WGSL/WESL files")
-    .option("rootDir", {
-      type: "string",
-      default: ".",
-      describe: "base directory of WGSL/WESL files",
-    })
-    .option("projectDir", {
-      type: "string",
-      default: ".",
-      describe: "directory containing package.json",
-    })
-    .option("updatePackageJson", {
-      type: "boolean",
-      default: true,
-      describe: "add export entries into package.json",
-    })
-    .option("outDir", {
-      type: "string",
-      default: "dist",
-      describe: "where to put bundled output files",
-    })
-    .help()
-    .parseSync();
+  return (
+    yargs(args)
+      .command("$0", "create an npm package from WGSL/WESL files")
+      .option("rootDir", {
+        type: "string",
+        default: ".",
+        describe: "base directory of WGSL/WESL files",
+      })
+      .option("projectDir", {
+        type: "string",
+        default: ".",
+        describe: "directory containing package.json",
+      })
+      // .option("updatePackageJson", {
+      //   type: "boolean",
+      //   default: true,
+      //   describe: "add export entries into package.json",
+      // })
+      .option("outDir", {
+        type: "string",
+        default: "dist",
+        describe: "where to put bundled output files",
+      })
+      .help()
+      .parseSync()
+  );
 }
