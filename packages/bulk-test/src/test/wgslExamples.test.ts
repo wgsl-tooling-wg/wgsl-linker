2b6aaae97fbee4fff1cdb580cd342209b232165d
lee mighdoll <lee@underneath.ca>
2024-11-05 23:00:25 -0800

    skip some wgslExamples files

--- packages/bulk-test/src/test/wgslExamples.test.ts
+++ packages/bulk-test/src/test/wgslExamples.test.ts
@@ -1,12 +1,14 @@
-import { test } from "vitest";
 import { glob } from "glob";
-import { dlog } from "berry-pretty";
-import fs  from "node:fs/promises";
+import fs from "node:fs/promises";
+import { test } from "vitest";
 import { ModuleRegistry } from "wgsl-linker";
 
-const wgslRoot = "../../../community-wgsl/webgpu-samples"
+const wgslRoot = "../../../community-wgsl/webgpu-samples";
 
-const texts = await loadFiles();
+// these require composing some wgsl files together, so just skip em for now
+const exclude = ["skinnedMesh", "cornell"];
+
+const texts = await loadFiles(exclude);
 
 texts.forEach(([path, text]) => {
   const testName = path.replace(wgslRoot, "");
@@ -16,15 +18,17 @@ texts.forEach(([path, text]) => {
   });
 });
 
-async function loadFiles(): Promise<[string, string][]> {
+async function loadFiles(exclude: string[]): Promise<[string, string][]> {
   const files = await glob(`${wgslRoot}/**/*.wgsl`, {
-    ignore: "node_modules/**",
+    ignore: ["node_modules/**"],
   });
-  const futureEntries = files.map(async path => {
+  const activeFiles = files.filter(
+    path => !exclude.some(e => path.includes(e)),
+  );
+  const futureEntries = activeFiles.map(async path => {
     const futureText = await fs.readFile(path, { encoding: "utf8" });
     return [path, futureText] as [string, string];
   });
   const entries = await Promise.all(futureEntries);
   return entries;
 }
-
