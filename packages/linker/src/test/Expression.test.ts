58f06e4bb8a1d1d1bfb15938b90c0ab3399cdf67
lee mighdoll <lee@underneath.ca>
2024-11-15 23:02:04 -0800

    add assertion and format

--- packages/linker/src/test/Expression.test.ts
+++ packages/linker/src/test/Expression.test.ts
@@ -1,8 +1,8 @@
-import { expect, test } from "vitest";
-import { testAppParse } from "./TestUtil.ts";
-import { expression } from "../ParseWgslD.ts";
-import { eof, seq } from "mini-parse";
 import { dlog } from "berry-pretty";
+import { eof, seq } from "mini-parse";
+import { expect, test } from "vitest";
+import { expression } from "../ParseWgslD.ts";
+import { testAppParse } from "./TestUtil.ts";
 
 test("parse number", () => {
   const src = `3`;
@@ -16,5 +16,6 @@ test.skip("parse comparisons with && ||", () => {
   const src = `array<3 && 4>(5)`;
   const { parsed } = testAppParse(seq(expression, eof), src);
   dlog({ parsed });
+  expect(parsed).not.toBeNull();
   expect(parsed!.tags.ident).toEqual(["array"]);
 });
