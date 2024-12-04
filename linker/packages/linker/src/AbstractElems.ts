bdb249aa25e5f69a86c943f7e56814ccb25a6602
lee mighdoll <lee@underneath.ca>
2024-12-03 20:19:05 -0800

    comment AbstractElems for some future changes

--- linker/packages/linker/src/AbstractElems.ts
+++ linker/packages/linker/src/AbstractElems.ts
@@ -31,17 +31,20 @@ export interface AbstractElemBase {
   end: number;
 }
 
+// legacy, to be removed
 export interface CallElem extends AbstractElemBase {
   kind: "call";
   name: string;
   ref?: FoundRef;
 }
 
+// legacy, to be removed
 export interface FnNameElem extends AbstractElemBase {
   kind: "fnName";
   name: string;
 }
 
+// legacy, to be removed
 export interface VarNameElem extends AbstractElemBase {
   kind: "varName";
   name: string;
@@ -49,18 +52,21 @@ export interface VarNameElem extends AbstractElemBase {
 
 export interface FnElem extends AbstractElemBase {
   kind: "fn";
-  name: string;
-  nameElem: FnNameElem;
-  calls: CallElem[];
-  typeRefs: TypeRefElem[];
+  name: string; // change to Ident?
+  nameElem: FnNameElem; // legacy, to be removed
+  calls: CallElem[]; // legacy, to be removed
+  typeRefs: TypeRefElem[]; // legacy, to be removed
+  elems?: ChildElemType[];
 }
 
+// legacy, to be removed
 export interface TypeRefElem extends AbstractElemBase {
   kind: "typeRef";
   name: string;
   ref?: FoundRef;
 }
 
+// legacy, to be removed
 export interface TypeNameElem extends AbstractElemBase {
   kind: "typeName";
   name: string;
@@ -68,17 +74,18 @@ export interface TypeNameElem extends AbstractElemBase {
 
 export interface StructElem extends AbstractElemBase {
   kind: "struct";
-  name: string;
-  nameElem: TypeNameElem;
+  name: string; // change to Ident?
+  nameElem: TypeNameElem; // legacy, to be removed
   members: StructMemberElem[];
 }
 
 export interface StructMemberElem extends AbstractElemBase {
   kind: "member";
-  name: string;
-  typeRefs: TypeRefElem[];
+  name: string; 
+  typeRefs: TypeRefElem[]; // change to RefIdent
 }
 
+// legacy, to be removed
 export interface ExportElem extends AbstractElemBase {
   kind: "export";
   args?: string[];
@@ -92,6 +99,7 @@ export interface TreeImportElem extends AbstractElemBase {
   imports: ImportTree;
 }
 
+// legacy support for the 'module' directive
 export interface ModuleElem extends AbstractElemBase {
   kind: "module";
   name: string;
@@ -99,16 +107,16 @@ export interface ModuleElem extends AbstractElemBase {
 
 export interface VarElem extends AbstractElemBase {
   kind: "var";
-  name: string;
-  nameElem: VarNameElem;
-  typeRefs: TypeRefElem[];
+  name: string; // change to DeclIdent?
+  nameElem: VarNameElem; // legacy, remove
+  typeRefs: TypeRefElem[]; // change to RefIdent
 }
 
 export interface AliasElem extends AbstractElemBase {
   kind: "alias";
-  name: string;
-  targetName: string;
-  typeRefs: TypeRefElem[];
+  name: string; // change to DeclIdent?
+  targetName: string; // change to RefIdent?
+  typeRefs: TypeRefElem[]; // legacy, remove
 }
 
 /** global directive (diagnostic, enable, requires) or const_assert */
@@ -116,10 +124,13 @@ export interface GlobalDirectiveElem extends AbstractElemBase {
   kind: "globalDirective";
 }
 
+/** an element that may be inside another element */
+export type ChildElemType = ChunkElem | IdentElem | TextElem;
+
 // an undifferentiated chunk of WESL source, contains other chunks and idents
 export interface ChunkElem extends AbstractElemBase {
   kind: "chunk";
-  elems: (ChunkElem | IdentElem | TextElem)[];
+  elems: ChildElemType[];
   conditions?: any; // TBD
 }
 
@@ -134,5 +145,5 @@ export interface IdentElem extends AbstractElemBase {
 */
 export interface TextElem extends AbstractElemBase {
   kind: "text";
-  src: SrcModule; // TODO move to abstract elem base
+  src: SrcModule; // TODO move to abstract elem base?
 }
