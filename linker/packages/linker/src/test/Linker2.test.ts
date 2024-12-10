a69a482f97b519d8bf7b7258ef70cf7e1da99442
lee mighdoll <lee@underneath.ca>
2024-12-09 21:20:37 -0800

    first linker2 test passes

--- linker/packages/linker/src/test/Linker2.test.ts
+++ linker/packages/linker/src/test/Linker2.test.ts
@@ -4,7 +4,5 @@ import { link2Test } from "./TestUtil.js";
 test("link global var", () => {
   const src = `var x: i32 = 1;`;
   const result = link2Test(src);
-  console.log("result:\n\n", result);
-
   expect(result).toEqual(src);
 });
