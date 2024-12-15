60e71278e756ff413ff58f954b4070ab73c0ac0b
lee mighdoll <lee@underneath.ca>
2024-12-15 11:30:08 +0800

    fix visit loop for fnChildren

--- linker/packages/mini-parse/src/SimplePretty.ts
+++ linker/packages/mini-parse/src/SimplePretty.ts
@@ -3,7 +3,7 @@ import { Parser } from "mini-parse";
 type AnyParser = Parser<any, any>;
 
 export function printParser(p: AnyParser): void {
-  fnChildrenDeep(p);
+  fnChildrenDeep(p, new Set());
   printDeep(p, 0, new Set());
 }
 
@@ -15,19 +15,20 @@ function printDeep(
   const pad = " ".repeat(indent);
   if (visited.has(p)) {
     console.log(pad + "->" + p.debugName);
-    return;
   } else {
     visited.add(p);
+    console.log(pad + p.debugName);
+    p._children?.forEach(c => printDeep(c, indent + 2, visited));
   }
-  console.log(pad + p.debugName);
-  p._children?.forEach(c => printDeep(c, indent + 2, visited));
 }
 
-function fnChildrenDeep(p: AnyParser): void {
-  if (p.debugName === "fn()") {
-    const newChild = (p as any)._fn() as AnyParser;
-    p._children = [newChild];
-  } else {
-    p._children?.forEach(fnChildrenDeep);
+function fnChildrenDeep(p: AnyParser, visited: Set<AnyParser>): void {
+  if (!visited.has(p)) {
+    visited.add(p);
+    if (p.debugName === "fn()") {
+      const newChild = (p as any)._fn() as AnyParser;
+      p._children = [newChild];
+    }
+    p._children?.forEach(c => fnChildrenDeep(c, visited));
   }
 }
