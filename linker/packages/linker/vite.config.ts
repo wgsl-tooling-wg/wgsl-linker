c6c1c41f2e8ae28b914fd41671ea577980347de3
lee mighdoll <lee@underneath.ca>
2025-01-05 05:51:48 -0800

    configure tests and tsc to rerun when tests change

--- linker/packages/linker/vite.config.ts
+++ linker/packages/linker/vite.config.ts
@@ -6,4 +6,8 @@ import { baseViteConfig } from "./base.vite.config.js";
 const config = baseViteConfig();
 config.test = { setupFiles: "./src/test/TestSetup.ts" };
 
+// so vitest reruns when tests change
+const aliasPath = new URL("../../../wesl-testsuite", import.meta.url).pathname;
+config.resolve = { alias: { "wesl-testsuite": aliasPath } };
+
 export default defineConfig(config);
