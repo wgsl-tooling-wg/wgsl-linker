0c79c57647e669d557bd60decc6f84cf62ab099b
lee mighdoll <lee@underneath.ca>
2025-01-08 14:07:25 -0800

    update package linking test to linker2 and random_wgsl

--- linker/packages/linker/src/test/LinkPackage.test.ts
+++ linker/packages/linker/src/test/LinkPackage.test.ts
@@ -1,15 +1,11 @@
 import { expectNoLog } from "mini-parse/test-util";
 import { expect, test } from "vitest";
-import lib from "wgsl-rand";
-import { ModuleRegistry } from "../ModuleRegistry.js";
+import lib from "random_wgsl";
+import { linkWeslFiles } from "../Linker2.ts";
 
-// TODO-lee this breaks because the current parser hacks
-// to produce calls or typeRefs are overzealous and confuse traverseRefs.
-// Current plan is to fix the linker to not expect calls and typeRefs.
-//
-test.skip("import rand() from a package", () => {
+test("import rand() from a package", () => {
   const src = `
-    import wgsl-rand/pcg_2u_3f; 
+    import random_wgsl/pcg_2u_3f; 
 
     struct Uniforms { frame: u32 }
     @binding(0) @group(0) var<uniform> u: Uniforms;
@@ -22,9 +18,9 @@ test.skip("import rand() from a package", () => {
   `;
 
   const wgsl = { "./main.wesl": src };
-  const result = expectNoLog(() => {
-    const registry = new ModuleRegistry({ wgsl, libs: [lib] });
-    return registry.link("./main");
-  });
-  expect(result).toContain("fn pcg_2u_3f");
+  const result = expectNoLog(() =>
+    linkWeslFiles(wgsl, "./main.wesl", {}, [lib]),
+  );
+  expect(result.dest).toContain("fn pcg_2u_3f");
+  expect(result.dest).not.toContain("sinRand");
 });
