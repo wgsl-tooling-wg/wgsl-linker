e61bd01937751c2c0598df3dde22d3c8be492ffd
lee mighdoll <lee@underneath.ca>
2025-01-09 09:00:37 -0800

    cosmetic

--- linker/packages/linker/src/CommentsGrammar.ts
+++ linker/packages/linker/src/CommentsGrammar.ts
@@ -14,8 +14,9 @@ import {
 } from "mini-parse";
 import { argsTokens, lineCommentTokens, mainTokens } from "./WESLTokens.ts";
 
-/* Basic parsing functions for comment handling, eol, etc. */
+/* Basic parsing functions for comment handling . */
 
+/** currently unused */
 export const unknown = any().map(r => {
   const { kind, text } = r.value;
   const deepName = r.ctx._debugNames.join(" > ");
@@ -24,19 +25,18 @@ export const unknown = any().map(r => {
   // throw new Error("Fail fast");
 });
 
-export const blockComment: Parser<any> = seq(
-  "/*",
-  repeat(or(() => blockComment, anyNot("*/"))),
-  req("*/"),
-);
-
 const eolf: Parser<any> = disablePreParse(
   makeEolf(argsTokens, argsTokens.ws),
 );
 
 const skipToEol = tokens(lineCommentTokens, anyThrough(eolf));
 
-/** parse a line comment */
+export const blockComment: Parser<any> = seq(
+  "/*",
+  repeat(or(() => blockComment, anyNot("*/"))),
+  req("*/"),
+);
+
 export const lineComment = seq(tokens(mainTokens, "//"), skipToEol);
 
 export const comment = or(() => lineComment, blockComment).trace({
