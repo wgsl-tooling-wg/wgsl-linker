0047f68c4f55a0c1d08990c2dbbef4a634464bb5
lee mighdoll <lee@underneath.ca>
2025-01-08 16:56:04 -0800

    rm some linker1 tests

--- linker/packages/linker/src/test/TestUtil.ts
+++ linker/packages/linker/src/test/TestUtil.ts
@@ -5,11 +5,8 @@ import {
   testParse,
   TestParseResult,
 } from "mini-parse/test-util";
-import { TaskContext } from "vitest";
-import { AbstractElem } from "../AbstractElems.js";
 import { linkWesl } from "../Linker2.js";
 import { mainTokens } from "../MatchWgslD.js";
-import { ModuleRegistry } from "../ModuleRegistry.js";
 import { parseWESL, syntheticWeslParseState, WeslAST } from "../ParseWESL.js";
 
 export function testAppParse<T, N extends TagRecord = NoTags>(
@@ -18,40 +15,6 @@ export function testAppParse<T, N extends TagRecord = NoTags>(
 ): TestParseResult<T, N, WeslAST> {
   const appState = syntheticWeslParseState();
   return testParse(parser, src, mainTokens, appState);
-}
-
-/** Convenience wrapper to link wgsl for tests.
- * The first file is named "root.wgsl", subsequent files are named "file1.wgsl", "file2.wgsl", etc.
- */
-export function linkTest(...rawWgsl: string[]): string {
-  return linkTestOpts({}, ...rawWgsl);
-}
-
-export interface LinkTestOpts {
-  runtimeParams?: Record<string, any>;
-}
-
-/** Convenience wrapper to link wgsl for tests, with load and link options. */
-export function linkTestOpts(opts: LinkTestOpts, ...rawWgsl: string[]): string {
-  const [root, ...rest] = rawWgsl;
-  const { runtimeParams } = opts;
-
-  const restWgsl = Object.fromEntries(
-    rest.map((src, i) => [`./file${i + 1}.wgsl`, src]),
-  );
-  const wgsl = { "./root.wgsl": root, ...restWgsl };
-
-  const registry = new ModuleRegistry({ wgsl });
-  return registry.link("./root", runtimeParams);
-}
-
-// TODO legacy test runner, remove
-export function testParseWgsl(src: string): AbstractElem[] {
-  return expectNoLog(() => parseWESL(src, undefined, 500).elems);
-}
-
-export function expectWgsl(ctx: TaskContext): void {
-  testParseWgsl(ctx.task.name);
 }
 
 /** Convenience wrapper to link wgsl for tests.
