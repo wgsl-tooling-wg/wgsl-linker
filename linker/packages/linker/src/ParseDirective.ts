25e680b426fe7f85ad28400e3b1595113466c5a0
lee mighdoll <lee@underneath.ca>
2025-01-08 20:29:47 -0800

    rm 'export' from parser
    future plan is for public by default, with a 'private' access control

--- linker/packages/linker/src/ParseDirective.ts
+++ linker/packages/linker/src/ParseDirective.ts
@@ -1,18 +1,16 @@
 import {
   anyThrough,
   kind,
-  opt,
   or,
   repeat,
-  req,
   seq,
   setTraceNames,
   tokens,
-  tracing,
+  tracing
 } from "mini-parse";
 import { gleamImport } from "./ImportGrammar.js";
-import { argsTokens, lineCommentTokens, mainTokens } from "./WESLTokens.js";
 import { eolf } from "./ParseSupport.js";
+import { argsTokens, lineCommentTokens, mainTokens } from "./WESLTokens.js";
 
 /* parse #directive enhancements to wgsl: #export, etc. */
 
@@ -21,12 +19,9 @@ const fromWord = or(argsWord, kind(argsTokens.relPath));
 
 const fromClause = seq("from", or(fromWord, seq('"', fromWord, '"')));
 
-/** #export <foo> <(a,b)> EOL */
-export const exportDirective = seq(or("#export", "export"), opt(eolf));
-
 export const directive = tokens(
   argsTokens,
-  seq(repeat("\n"), or(exportDirective, gleamImport)),
+  seq(repeat("\n"), or(gleamImport)),
 );
 
 const skipToEol = tokens(lineCommentTokens, anyThrough(eolf));
@@ -37,7 +32,6 @@ export const lineComment = seq(tokens(mainTokens, "//"), skipToEol);
 if (tracing) {
   setTraceNames({
     fromClause,
-    exportDirective,
     skipToEol,
     lineComment,
     directive,
