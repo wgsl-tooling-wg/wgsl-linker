49414c1dbd41116006019737315e38363e7d590c
lee mighdoll <lee@underneath.ca>
2025-01-08 17:04:09 -0800

    rm most of linker1

--- linker/packages/linker/src/index.ts
+++ linker/packages/linker/src/index.ts
@@ -1,7 +1,5 @@
 export * from "./ASTLogging.js";
-export * from "./Linker.js";
 export * from "./Linker2.js";
-export * from "./ModuleRegistry.js";
 export * from "./ParseWESL.js";
 export * from "./PathUtil.js";
 export * from "./Util.js";
