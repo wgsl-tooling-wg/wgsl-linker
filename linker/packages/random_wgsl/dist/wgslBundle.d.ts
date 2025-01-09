7ec932d97c22cc0e12291b8d832eb27f4be19d13
lee mighdoll <lee@underneath.ca>
2025-01-08 20:27:13 -0800

    include generated random_wgsl bundle in git

---
+++ linker/packages/random_wgsl/dist/wgslBundle.d.ts
@@ -0,0 +1,19 @@
+export interface WgslBundle {
+  /** name of the package, e.g. wgsl-rand */
+  name: string;
+
+  /** npm version of the package  e.g. 0.4.1 */
+  version: string;
+
+  /** wesl edition of the code e.g. wesl_unstable_2024_1 */
+  edition: string;
+
+  /** map of wesl/wgsl modules:
+   *    keys are file paths, relative to package root (e.g. "./lib.wgsl")
+   *    values are wgsl/wesl code strings
+   */
+  modules: Record<string, string>;
+}
+
+export declare const wgslBundle: WgslBundle;
+export default wgslBundle;
