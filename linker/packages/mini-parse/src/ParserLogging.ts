08b650992b4e92e747151b56fa4dfdc0b8eb80b7
lee mighdoll <lee@underneath.ca>
2025-01-11 10:01:16 -0800

    mark todo

--- linker/packages/mini-parse/src/ParserLogging.ts
+++ linker/packages/mini-parse/src/ParserLogging.ts
@@ -99,6 +99,7 @@ function carets(linePos: number, linePos2?: number): string {
   const firstCaret = " ".repeat(linePos) + "^";
   let secondCaret = "";
   if (linePos2 && linePos2 > linePos) {
+    // TODO maybe a continuous line of carets would look nicer?
     secondCaret = " ".repeat(linePos2 - linePos - 1) + "^";
   }
   return firstCaret + secondCaret;
