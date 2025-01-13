1beaf8aa9ef8a8b0f04668f994f90d7b4a96a59a
lee mighdoll <lee@underneath.ca>
2025-01-13 07:50:18 -0800

    comment and alphabetize

--- linker/packages/linker/src/AbstractElems.ts
+++ linker/packages/linker/src/AbstractElems.ts
@@ -14,23 +14,23 @@ import { DeclIdent, RefIdent, SrcModule } from "./Scope.ts";
 export type GrammarElem =
   | AliasElem
   | AttributeElem
-  | ConstElem
-  | ExpressionElem
-  | ImportElem
   | ConstAssertElem
-  | FnElem
-  | RefIdentElem
+  | ConstElem
   | DeclIdentElem
+  | ExpressionElem
+  | FnElem
+  | GlobalVarElem
+  | ImportElem
   | ModuleElem
   | NameElem
   | OverrideElem
   | ParamElem
+  | RefIdentElem
   | SimpleMemberRef
   | StructElem
   | StructMemberElem
   | TextElem
   | TypeRefElem
-  | GlobalVarElem
   | VarElem;
 
 export type AbstractElem = GrammarElem | SyntheticElem;
@@ -38,11 +38,11 @@ export type AbstractElem = GrammarElem | SyntheticElem;
 export type DeclarationElem =
   | AliasElem
   | ConstElem
+  | FnElem
+  | GlobalVarElem
   | OverrideElem
   | ParamElem
-  | FnElem
   | StructElem
-  | GlobalVarElem
   | VarElem;
 
 export interface AbstractElemBase {
@@ -55,59 +55,51 @@ export interface ElemWithContents extends AbstractElemBase {
   contents: AbstractElem[];
 }
 
-/** a raw bit of text in WESL source that's typically copied to the linked WGSL. 
- e.g. a keyword  like 'var' 
- or a phrase we needn't analyze further like '@diagnostic(off,derivative_uniformity)'
-*/
+
+/* ------   Terminal Elements  (don't contain other elements)  ------   */
+
+/**
+ * a raw bit of text in WESL source that's typically copied to the linked WGSL.
+ * e.g. a keyword  like 'var'
+ * or a phrase we needn't analyze further like '@diagnostic(off,derivative_uniformity)'
+ */
 export interface TextElem extends AbstractElemBase {
   kind: "text";
   srcModule: SrcModule;
 }
 
-export interface ImportElem extends ElemWithContents {
-  kind: "import";
-  imports: ImportTree;
+/** a name (e.g. a struct member name) that doesn't need to be an Ident */
+export interface NameElem extends AbstractElemBase {
+  kind: "name";
+  name: string;
+  srcModule: SrcModule;
 }
 
-/** simple references to structures, like myStruct.bar
- * (used for transforming refs to binding structs) */
-export interface SimpleMemberRef extends ElemWithContents {
-  kind: "memberRef";
-  name: RefIdentElem;
-  member: NameElem;
-}
-
-export type TypeTemplateParameter = TypeRefElem | ExpressionElem | string;
-
-/** an expression (generally we don't need details of expressions, just their contained idents */
-export interface ExpressionElem extends ElemWithContents {
-  kind: "expression";
-}
-
-/** a reference to a type, like 'f32', or 'MyStruct', or 'ptr<storage, array<f32>, read_only>'   */
-export interface TypeRefElem extends ElemWithContents {
-  kind: "type";
-  name: RefIdent | string;
-  templateParams?: TypeTemplateParameter[];
-}
-
-/** an identifier that refers to a declaration */
+/** an identifier that refers to a declaration (aka a symbol reference) */
 export interface RefIdentElem extends AbstractElemBase {
   kind: RefIdent["kind"];
   ident: RefIdent;
   srcModule: SrcModule;
 }
 
-/** a declaration identifier */
+/** a declaration identifier (aka a symbol declaration) */
 export interface DeclIdentElem extends AbstractElemBase {
   kind: DeclIdent["kind"];
   ident: DeclIdent;
   srcModule: SrcModule;
 }
 
-/** a parameter in a function declaration */
-export interface ParamElem extends ElemWithContents {
-  kind: "param";
+/** generated element, produced after parsing and binding */
+export interface SyntheticElem {
+  kind: "synthetic";
+  text: string;
+}
+
+/* ------   Container Elements  (don't contain other elements)  ------   */
+
+/** an alias statement */
+export interface AliasElem extends ElemWithContents {
+  kind: "alias";
   name: DeclIdentElem;
   typeRef: TypeRefElem;
 }
@@ -119,18 +111,9 @@ export interface AttributeElem extends ElemWithContents {
   params?: ExpressionElem[];
 }
 
-/** a variable declaration */
-export interface VarElem extends ElemWithContents {
-  kind: "var";
-  name: DeclIdentElem;
-  typeRef?: TypeRefElem;
-}
-
-/** a global variable declaration (at the root level) */
-export interface GlobalVarElem extends ElemWithContents {
-  kind: "gvar";
-  name: DeclIdentElem;
-  typeRef?: TypeRefElem;
+/** a const_assert statement */
+export interface ConstAssertElem extends ElemWithContents {
+  kind: "assert";
 }
 
 /** a const declaration */
@@ -140,11 +123,30 @@ export interface ConstElem extends ElemWithContents {
   typeRef?: TypeRefElem;
 }
 
-/** an override declaration */
-export interface OverrideElem extends ElemWithContents {
-  kind: "override";
+/** an expression (generally we don't need details of expressions, just their contained idents) */
+export interface ExpressionElem extends ElemWithContents {
+  kind: "expression";
+}
+
+/** a function declaration */
+export interface FnElem extends ElemWithContents {
+  kind: "fn";
+  name: DeclIdentElem;
+  params: ParamElem[];
+  returnType?: TypeRefElem;
+}
+
+/** a global variable declaration (at the root level) */
+export interface GlobalVarElem extends ElemWithContents {
+  kind: "gvar";
   name: DeclIdentElem;
   typeRef?: TypeRefElem;
+}
+
+/** an import statement */
+export interface ImportElem extends ElemWithContents {
+  kind: "import";
+  imports: ImportTree;
 }
 
 /** an entire file */
@@ -152,16 +154,26 @@ export interface ModuleElem extends ElemWithContents {
   kind: "module";
 }
 
-/** an alias statement */
-export interface AliasElem extends ElemWithContents {
-  kind: "alias";
+/** an override declaration */
+export interface OverrideElem extends ElemWithContents {
+  kind: "override";
+  name: DeclIdentElem;
+  typeRef?: TypeRefElem;
+}
+
+/** a parameter in a function declaration */
+export interface ParamElem extends ElemWithContents {
+  kind: "param";
   name: DeclIdentElem;
   typeRef: TypeRefElem;
 }
 
-/** a const_assert statement */
-export interface ConstAssertElem extends ElemWithContents {
-  kind: "assert";
+/** simple references to structures, like myStruct.bar
+ * (used for transforming refs to binding structs) */
+export interface SimpleMemberRef extends ElemWithContents {
+  kind: "memberRef";
+  name: RefIdentElem;
+  member: NameElem;
 }
 
 /** a struct declaration */
@@ -180,22 +192,18 @@ export interface StructMemberElem extends ElemWithContents {
   typeRef: TypeRefElem;
 }
 
-/** a name (e.g. a struct member name) that doesn't need to be an Ident */
-export interface NameElem extends AbstractElemBase {
-  kind: "name";
-  name: string;
-  srcModule: SrcModule;
+export type TypeTemplateParameter = TypeRefElem | ExpressionElem | string;
+
+/** a reference to a type, like 'f32', or 'MyStruct', or 'ptr<storage, array<f32>, read_only>'   */
+export interface TypeRefElem extends ElemWithContents {
+  kind: "type";
+  name: RefIdent | string;
+  templateParams?: TypeTemplateParameter[];
 }
 
-/** a function declaration */
-export interface FnElem extends ElemWithContents {
-  kind: "fn";
+/** a variable declaration */
+export interface VarElem extends ElemWithContents {
+  kind: "var";
   name: DeclIdentElem;
-  params: ParamElem[];
-  returnType?: TypeRefElem;
-}
-
-export interface SyntheticElem {
-  kind: "synthetic";
-  text: string;
+  typeRef?: TypeRefElem;
 }
